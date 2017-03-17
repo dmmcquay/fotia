3ade7fa63aac9ee1d1052e2c2f634b3d8e18e425
Derek McQuay <dmcquay@apprenda.com>
2017-03-16 17:39:17 -0700

    added "silly" route
    will probably move towards this being a gauge

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
 	"runtime"
+	"strings"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -48,6 +50,18 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
+type failure struct {
+	Success bool   `json:"success"`
+	Error   string `json:"error"`
+}
+
+func NewFailure(msg string) *failure {
+	return &failure{
+		Success: false,
+		Error:   msg,
+	}
+}
+
 // Time is a function that makes it simple to add one-line timings to function
 // calls.
 func Time() func() {
@@ -76,10 +90,27 @@ func sleep(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(time.Second * time.Duration(i))
 }
 
+func silly(w http.ResponseWriter, r *http.Request) {
+	searchreq := r.URL.Path[len("/silly/"):]
+	if len(searchreq) == 0 {
+		b, _ := json.Marshal(NewFailure("url could not be parsed"))
+		http.Error(w, string(b), http.StatusBadRequest)
+		return
+	}
+	if searchreq[len(searchreq)-1] != '/' {
+		http.Redirect(w, r, "/silly/"+searchreq+"/", http.StatusMovedPermanently)
+		return
+	}
+	searchReqParsed := strings.Split(searchreq, "/")
+	//Msgs.WithLabelValues("silly").Observe(searchReqParsed[0])
+	fmt.Fprintf(w, fmt.Sprintf("%s\n", searchReqParsed[0]))
+}
+
 func main() {
 	flag.Parse()
 	http.Handle("/metrics", prometheus.Handler())
 	http.HandleFunc("/demo", demo)
 	http.HandleFunc("/sleep", sleep)
+	http.HandleFunc("/silly/", silly)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
